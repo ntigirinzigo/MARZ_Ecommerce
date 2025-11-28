530a2923f5c782f1c012b2d64b2b7d8560aecb72
bukira sophonie <bukirasophonie347@gmail.com>
2025-11-28 13:15:33 -0500

    ntigirinzigo project ecommerce website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/MARZ_Ecommerce/', // Change to your repo name! (e.g., /your-repo-name/)
 })
